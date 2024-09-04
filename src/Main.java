8408131658389790418ba22a8f31aaa2701f5bc7
EmilieTossan <emilietossan@gmail.com>
2024-09-04 15:10:50 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        List<Voiture> listeVoitures = Arrays.asList(
+            new Voiture("Peugeot", "Traveller"),
+            new Voiture("Peugeot", "Rifter"),
+            new Voiture("Peugeot", "Partner"),
+            new Voiture("Renault", "Megane"),
+            new Voiture("Renault", "Kangoo"),
+            new Voiture("Renault", "Clio"),
+            new Voiture("Renault", "Zoe"),
+            new Voiture("Citroën", "Berlingo")
+        );
+
+        listeVoitures.stream()
+            .map(Voiture::getMarque)
+            .filter(v -> !v.equals("Peugeot"))
+            .distinct()
+            .forEach(System.out::println);
+
+        listeVoitures.stream()
+            .map(Voiture::getMarque)
+            .filter(v -> v.startsWith("P"))
+            .distinct()
+            .forEach(System.out::println);
+    }
+}
---
+++ src/Voiture.java
@@ -0,0 +1,13 @@
+public class Voiture {
+    private String marque;
+    private String modele;
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public Voiture (String marque, String modele) {
+        this.marque = marque;
+        this.modele = modele;
+    }
+}
